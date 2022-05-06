aa557e97e780c482d11086688b628bb7411d379c
Galahad <steam_team_look@mail.ru>
2022-05-06 17:20:30 +0300

    Chuck Norris

---
+++ index.js
@@ -0,0 +1,25 @@
+const submit = document.querySelector(".submit");
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const list = document.querySelector("#list");
+  list.style.borderColor = "black";
+  list.textContent = "";
+
+  const img = document.createElement("img");
+  img.src = "https://www.kino-teatr.ru/acter/album/50064/34947.jpg";
+  img.alt = "Сргсл Norris";
+  img.classList.add("img");
+
+  const textInfo = document.createElement("p");
+  textInfo.classList.add("text_info");
+
+  process(textInfo);
+
+  list.append(img, textInfo);
+});
+
+const process = async (value) => {
+  const res = await fetch("https://api.chucknorris.io/jokes/random");
+  const type = await res.json();
+  value.textContent = type.value;
+};
